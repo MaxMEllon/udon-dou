92297a031bda7a0e0399a49df9a34041304dae19
MaxMEllon <mozi_kke@excite.co.jp>
2015-09-25 19:22:32 +0900

    :tada: first commit
    :gem: Add: gems
    
    :art: Add: css framework (git submodule)
    
    :memo: Update: readme, Add: logo, LICENSE
    
    :boom: Remove: gitsubmodule of uikit and material-design
    
    gulpでbuildする必要があったり，jsとcssをassets以下に置き直す必要が有るため
    uikitはgem，material-designはbuild住みを設置
    
    :art: Add: uikit and material-design-lite
    
    :wrench: Add: config

---
+++ README.md
@@ -0,0 +1,49 @@
+<p align="center">
+  <img src="./public/logo.png">
+</p>
+ロゴは仮です
+- - -
+
+## Description
+
+`うどんどう`は，うどん好きがつくるうどん好きのためのうどん口コミサービスです．
+さぬきうどんだけではなく，全国のうどんに焦点を当てます．
+また，ほうとうやきしめんなどをうどんにいれるかどうかの議論・討論する場を
+提供します
+
+## Features
+
+## Usage
+
+## Requirement
+
+- ruby  2.2.0
+- rails 2.2.3
+- gems : Gemfile を参照してください
+- postgresql
+- material-design-light
+- uikit
+
+## How to run
+
+```sh
+$ git clone https://github.com/MaxMEllon/udon-dou.git
+$ bundle install
+$ rake db:create
+$ rake db:migrate
+$ rails s
+```
+
+## Author
+
+[@maxmellon](https://github.com/MaxMEllon)
+
+## LICENSE
+
+This software is released under the MIT License, see LICENSE.txt.
+
+:memo: TODO:
+
+- [ ] 必要最低限の機能が実装されたら rebaseでcommitをひとまとめに
+- [ ] ロゴを書く
+
---
+++ config/application.rb
@@ -0,0 +1,41 @@
+require File.expand_path('../boot', __FILE__)
+
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+# require "rails/test_unit/railtie"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module UdonMitchi
+  class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+    config.preferred_syntax = :sass
+    config.sass.syntax = :sass
+
+    config.i18n.default_locale = :ja
+
+    config.generators do |g|
+      g.template_engine = :slim
+      g.helper          = false
+      g.assets          = false
+      g.test_framework  = 'rspec'
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
+
+    config.git_revision = `git log --abbrev-commit --pretty=oneline | head -1 | cut -d' ' -f1`
+    config.autoload_paths += %W(#{config.root}/lib)
+    config.active_record.raise_in_transactional_callbacks = true
+    config.log_tags = [:remote_ip]
+  end
+end
