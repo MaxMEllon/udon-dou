8aad06a1f1e8656e1acdf4201230472e9c55707c
MaxMEllon <mozi_kke@excite.co.jp>
2015-10-08 23:47:16 +0900

    :clipboard: model documents by annotate

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: shops
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  description :text
+#  address     :string
+#  latitude    :float
+#  longitude   :float
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Shop < ActiveRecord::Base
   has_many :comments
 end
--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  shop_id    :integer
+#  user_id    :integer
+#  comment    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 FactoryGirl.define do
   factory :comment do
   end
